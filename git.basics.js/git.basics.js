aec609393e4626055f514b32688e578c47cef9c6
TusharBhardwaj47 <tusharbhardwaj635@gmail.com>
2026-01-09 11:15:00 +0530

    Deleteing commands and etc

---
+++ git.basics.js/git.basics.js
@@ -0,0 +1,44 @@
+console.log("Welcome to Git Basics!");
+console.log("This is a simple JavaScript file to demonstrate Git basics.");
+console.log("You can modify this file and track changes using Git.");
+
+
+
+/*
+Installing Git
+brew install git
+git --version 
+*/
+
+/*
+git config --global user.name "Your Name"
+git config --global user.email "your.email@example.com"
+*/
+
+/*
+git clone https://github.com/username/repository.git
+git clone https://github.com/username/repository.git new-folder-name
+*/
+
+/*
+git status
+git add .
+git commit -m "Your commit message"
+git push origin main
+*/
+
+/*
+git pull origin main
+*/
+
+
+//how to commit versions to a website
+/*
+git rm -n //file.txt
+git rm //file.txt
+commit -m "Removed file.txt"
+push origin main
+
+git rm src/old-file.js ----->Removing file
+git rmf src/old-file.js ----->Removing folder
+*/
